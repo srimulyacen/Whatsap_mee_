e7c86aa02dc678a7615272a7d7ee8e99993d9c59
srimulyacen <sri.mulyacen@gmail.com>
2026-06-07 20:57:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    profileImage: "profil.jpg",
+
+    backgrounds: [
+        "bg1.jpg",
+        "bg2.jpg"
+    ],
+
+    whatsappLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2173856&click_id=79kmph",
+    telegramLink: "https://kereva.heavengiirls.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=236468&s2=2173856&click_id=79kmph",
+
+    minMembers: 10000,
+    maxMembers: 15000,
+
+    minActive: 2500,
+    maxActive: 4000
+};
